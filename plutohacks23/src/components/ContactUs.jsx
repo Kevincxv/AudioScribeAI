86bb26e81f1303763a6d1c1476504d3846b1099b
Daniel E. Gonzalez <dgonz.cs@gmail.com>
2023-10-14 11:24:18 -0400

    Update the code

--- plutohacks23/src/components/ContactUs.jsx
+++ plutohacks23/src/components/ContactUs.jsx
@@ -1,96 +1,39 @@
-// import '../styles/ContactUsStyle.css';
-
-// function ContactUs() {
-//   return (
-//     <div id="contact-us" className="contact-us">
-//       <h1>Contact Us</h1>
-//       <form>
-//         <label htmlFor="name">Name:</label>
-//         <input type="text" id="name" name="name" />
-
-//         <label htmlFor="email">Email:</label>
-//         <input type="email" id="email" name="email" />
-
-//         <label htmlFor="message">Message:</label>
-//         <textarea id="message" name="message"></textarea>
-
-//         <button type="submit">Send</button>
-//       </form>
-//     </div>
-//   );
-// }
-
-// export default ContactUs;
-
-
-
-
-
-
-
-
-
-
-
-// import React, { useState } from 'react';
-// import '../styles/ContactUsStyle.css'; // You can create a new CSS file for ContactUs styling
-
-// function ContactUs() {
-//   const [name, setName] = useState('');
-
-//   const handleNameChange = (e) => {
-//     setName(e.target.value);
-//   };
-
-//   return (
-//     <div className="contact-us-container"> 
-//       <div className="box">
-//         <h3>Contact Us</h3>
-//         <input
-//           type="text"
-//           value={name}
-//           onChange={handleNameChange}
-//           placeholder="Enter your name"
-//         />
-//       </div>
-//       <a href="#">Employee? Click Here</a>
-//     </div>
-//   );
-// }
-
-// export default ContactUs;
-
-
-
-
-
+//ContactUs.jsx
 import React, { useState } from 'react';
-import '../styles/ContactUsStyle.css';
+import '../styles/FrontPageStyle.css';
 
-function ContactUs({ onNameChange }) {
-    const [name, setName] = useState('');
+function ContactUs({ onNameChange, onEmployeeClick }) {
+  const [name, setName] = useState('');
 
-    const handleNameChange = (e) => {
-        setName(e.target.value);
-        if (onNameChange) {
-            onNameChange(e.target.value); // Notify about the name change
-        }
-    };
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+    if (onNameChange) {
+      onNameChange(e.target.value); // Notify about the name change
+    }
+  };
 
-    return (
-        <div className="contact-us-container"> 
-            <div className="box">
-                <h3>Contact Us</h3>
-                <input
-                    type="text"
-                    value={name}
-                    onChange={handleNameChange}
-                    placeholder="Enter your name"
-                />
+  const handleButtonClick = () => {
+    // Handle button click event
+    // used to handle the button click event
+  };
+
+  return (
+    <div className="contact-us-container">
+      <div className="box-container">
+        <h3>Contact Us</h3>
+            <div className="name-input-container">
+            <input
+                type="text"
+                value={name}
+                onChange={handleNameChange}
+                placeholder="Enter your name"
+            />
             </div>
-            <a href="#">Employee? Click Here</a>
+            <button onClick={handleButtonClick}>Enter</button>
+            <button onClick={onEmployeeClick}>Employee? Click Here</button>
         </div>
-    );
+    </div>
+  );
 }
 
 export default ContactUs;
--- plutohacks23/src/components/Home.jsx
+++ plutohacks23/src/components/Home.jsx
@@ -1,12 +1,13 @@
-// import React from 'react';
-// import logo from '../images/logo.png';
-import '../styles/HomeStyle.css';
+import React from 'react';
+import '../styles/FrontPageStyle.css';
 
 function Home() {
   return (
-    <div id="home" className="home">
-      <h1>Welcome to our website</h1>
-      <p>We are a team of developers working on a cool project.</p>
+    <div className="home-container">
+      <div className="home">
+        <h1>AudioScribeAI</h1>
+        <p>We are a team of developers working on a cool project.</p>
+      </div>
     </div>
   );
 }

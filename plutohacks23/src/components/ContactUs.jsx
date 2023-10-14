2eb481e98723f9131625ce0660fe5b0e37477a53
NassG1214 <giancarlonassi@gmail.com>
2023-10-13 23:41:58 -0400

    Update

--- plutohacks23/src/components/ContactUs.jsx
+++ plutohacks23/src/components/ContactUs.jsx
@@ -1,95 +1,31 @@
-// import '../styles/ContactUsStyle.css';
-
-// function ContactUs() {
-//   return (
-//     <div id="contact-us" className="contact-us">
-//       <h1>Contact Us</h1>
-//       <form>
-//         <label htmlFor="name">Name:</label>
-//         <input type="text" id="name" name="name" />
-
-//         <label htmlFor="email">Email:</label>
-//         <input type="email" id="email" name="email" />
-
-//         <label htmlFor="message">Message:</label>
-//         <textarea id="message" name="message"></textarea>
-
-//         <button type="submit">Send</button>
-//       </form>
-//     </div>
-//   );
-// }
-
-// export default ContactUs;
-
-
-
-
-
-
-
-
-
-
-
-// import React, { useState } from 'react';
-// import '../styles/ContactUsStyle.css'; // You can create a new CSS file for ContactUs styling
-
-// function ContactUs() {
-//   const [name, setName] = useState('');
-
-//   const handleNameChange = (e) => {
-//     setName(e.target.value);
-//   };
-
-//   return (
-//     <div className="contact-us-container"> 
-//       <div className="box">
-//         <h3>Contact Us</h3>
-//         <input
-//           type="text"
-//           value={name}
-//           onChange={handleNameChange}
-//           placeholder="Enter your name"
-//         />
-//       </div>
-//       <a href="#">Employee? Click Here</a>
-//     </div>
-//   );
-// }
-
-// export default ContactUs;
-
-
-
-
-
 import React, { useState } from 'react';
-import '../styles/ContactUsStyle.css';
 
 function ContactUs({ onNameChange }) {
     const [name, setName] = useState('');
 
-    const handleNameChange = (e) => {
-        setName(e.target.value);
-        if (onNameChange) {
-            onNameChange(e.target.value); // Notify about the name change
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            const response = await fetch('http://localhost:5173/');
+            const data = await response.json();
+            onNameChange(data.name);
+        } catch (error) {
+            console.error(error);
         }
     };
 
+    const handleChange = (event) => {
+        setName(event.target.value);
+    };
+
     return (
-        <div className="contact-us-container"> 
-            <div className="box">
-                <h3>Contact Us</h3>
-                <input
-                    type="text"
-                    value={name}
-                    onChange={handleNameChange}
-                    placeholder="Enter your name"
-                />
-            </div>
-            <a href="#">Employee? Click Here</a>
-        </div>
+        <form onSubmit={handleSubmit}>
+            <label>
+                Name:
+                <input type="text" value={name} onChange={handleChange} />
+            </label>
+            <button type="submit">Submit</button>
+        </form>
     );
 }
 
--- plutohacks23/src/components/DemoIndex.jsx
+++ plutohacks23/src/components/DemoIndex.jsx
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../styles/demoIndex.css';
 import images from '../assets/images/profile-pic-icon.png';
 import answerIcon from '../assets/images/accept.svg'; 
 import declineIcon from '../assets/images/decline.svg';
 
-
 function DemoIndex() {
-    // const [callerName, setCallerName] = useState('');
+    const [callerName, setCallerName] = useState('');
+    const [isLoading, setIsLoading] = useState(true);
+    const [isAnswered, setIsAnswered] = useState(false);
+    const [isDeclined, setIsDeclined] = useState(false);
+
     const startRecording = async () => {
         // Implement the function to start recording (from the previous example)
     };
@@ -15,18 +18,55 @@ function DemoIndex() {
         // Implement the function to stop recording (from the previous example)
     };
 
+    const handleAnswerClick = () => {
+        setIsAnswered(true);
+    };
+
+    const handleDeclineClick = () => {
+        setIsDeclined(true);
+    };
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch('http://localhost:5173/');
+                const data = await response.json();
+                setCallerName(data.name);
+                setIsLoading(false);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    useEffect(() => {
+        if (isDeclined) {
+            setIsAnswered(false);
+            setIsDeclined(false);
+        }
+    }, [isDeclined]);
+
     return (
         <div className="profile-container">
-            {/* <h2>{callerName ? `${callerName} is calling...` : 'Waiting for a call...'}</h2> */}
+            {isLoading ? (
+                <h2>Loading...</h2>
+            ) : (
+                <h2>{callerName ? `${callerName} is calling...` : 'Waiting for a call...'}</h2>
+            )}
             <img src={images} alt="Profile" className="profile-pic" />
             <div className="button-container">
-                <button className="answer" onClick={startRecording}>
-                    <img src={answerIcon} alt="Answer" />
-                </button>
-                <button className="decline" onClick={stopRecording}>
-                    <img src={declineIcon} alt="Decline" />
-                </button>
-                {/* <ContactUs onNameChange={setCallerName} /> */}
+                {!isAnswered && !isDeclined && (
+                    <button className="answer" onClick={handleAnswerClick}>
+                        <img src={answerIcon} alt="Answer" />
+                    </button>
+                )}
+                {!isDeclined && (
+                    <button className="decline" onClick={handleDeclineClick}>
+                        <img src={declineIcon} alt="Decline" />
+                    </button>
+                )}
             </div>
         </div>
     );

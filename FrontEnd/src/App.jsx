702a663c9b2327e336b2d771440b822b913227c0
NassG1214 <giancarlonassi@gmail.com>
2023-10-14 11:09:10 -0400

    Added vibration

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/demo" element={<Demo />} />
         </Routes>
-      </BrowserRouter>
+      </BrowserRouter> 
     </>
   );
 }
--- FrontEnd/src/components/DemoIndex.jsx
+++ FrontEnd/src/components/DemoIndex.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../styles/demoIndex.css';
 import images from '../assets/images/profile-pic-icon.png';
 import answerIcon from '../assets/images/accept.svg'; 
@@ -7,14 +7,17 @@ import DemoPost from './DemoPost';
 
 function DemoIndex() {
     const [isAnswered, setIsAnswered] = useState(false);
-    const [isDeclined, setIsDeclined] = useState(false);
-    const [callerName, setCallerName] = useState('');
+    const [endCall, setEndCall] = useState(false);
+    const [isPaused, setIsPaused] = useState(false);
+    
+    const ringAudio = new Audio('../assests/latest_recording.wav');
 
     const startRecording = async () => {
         try {
             const response = await fetch('/start_recording', { method: 'POST' });
             const data = await response.json();
             console.log(data.status);
+            ringAudio.play(); // Play ringing sound
         } catch (err) {
             console.error(err.message);
         }
@@ -25,6 +28,7 @@ function DemoIndex() {
             const response = await fetch('/stop_recording', { method: 'POST' });
             const data = await response.json();
             console.log(data.status);
+            setEndCall(true); // Set endCall to true
         } catch (err) {
             console.error(err.message);
         }
@@ -36,25 +40,36 @@ function DemoIndex() {
     };
 
     const handleDeclineClick = () => {
-        setIsDeclined(true);
-        stopRecording();
+        if (isAnswered) {
+            stopRecording();
+        }
+        setEndCall(true); // Set endCall to true
     };
+
+    useEffect(() => {
+        if (endCall && !isAnswered) {
+            setIsPaused(true); // Pause animation
+            setTimeout(() => {
+                setIsPaused(false); // Resume animation after 3 seconds
+            }, 3000);
+        }
+    }, [endCall, isAnswered]);
 
     return (
         <>
-            {isDeclined ? (
-                <DemoPost name={callerName} />
+            {isAnswered && endCall ? (
+                <DemoPost />
             ) : (
                 <div className="profile-container">
                     <h2>Someone is calling...</h2>
-                    <img src={images} alt="Profile" className="profile-pic" />
+                    <img src={images} alt="Profile" className={`profile-pic ${isPaused ? 'paused' : ''}`} />
                     <div className="button-container">
-                        {!isAnswered && !isDeclined && (
+                        {!isAnswered && !endCall && (
                             <button className="answer" onClick={handleAnswerClick}>
                                 <img src={answerIcon} alt="Answer" />
                             </button>
                         )}
-                        {!isDeclined && (
+                        {!endCall && (
                             <button className="decline" onClick={handleDeclineClick}>
                                 <img src={declineIcon} alt="Decline" />
                             </button>
